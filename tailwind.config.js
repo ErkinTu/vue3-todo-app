c712189bcdbd90a2d8605cd988dc29cde3454b86
Erkin <erkint@gmail.com>
2025-04-14 15:13:29 +0600

    ♻ Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//         "./index.html",
+//         "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [],
+// }
 
+// Для старой версии tailwindcss@3.3.0 мы используем более устаревший синтаксис module.exports
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
