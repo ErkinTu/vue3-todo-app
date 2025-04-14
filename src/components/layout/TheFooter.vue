4867699592cea7f1f655a624ee60e90d45dde44d
Erkin <erkint@gmail.com>
2025-04-14 16:30:18 +0600

    🍀 Delete unnecessary word

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="bg-gray-100 border-t border-gray-200 py-4 mt-8">
     <div class="container mx-auto px-4 text-center text-gray-600 text-sm">
       <p>Vue 3 ToDo App © {{ new Date().getFullYear() }}</p>
-      <p class="mt-1">ErT. Создано с использованием Vue 3, Pinia, Vue Router и Tailwind CSS</p>
+      <p class="mt-1">Создано с использованием Vue 3, Pinia, Vue Router и Tailwind CSS</p>
     </div>
   </footer>
 </template>
